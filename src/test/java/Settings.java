1a869a3bfd7ce369a6be820e265818d3b3e68c6b
turepinar <yilmazerp@gmail.com>
2023-12-01 14:20:33 -0600

    second commit to Settings

--- src/test/java/Settings.java
+++ src/test/java/Settings.java
@@ -1,5 +1,6 @@
 public class Settings {
     public static void main(String[] args) {
         System.out.println("Setting class created");
+        System.out.println("Second commit to Settings");
     }
 }
