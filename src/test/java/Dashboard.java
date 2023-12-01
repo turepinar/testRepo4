dafc750ece9553ddfc022a7c5df0797bc0b4dd50
turepinar <yilmazerp@gmail.com>
2023-12-01 10:50:52 -0600

    second commit before pull request

--- src/test/java/Dashboard.java
+++ src/test/java/Dashboard.java
@@ -1,5 +1,6 @@
 public class Dashboard {
     public static void main(String[] args) {
         System.out.println("My new dashboard is here");
+        System.out.println("second commit is coming without creating a pull request for this branch ");
     }
 }
