0e52bff415d5e165393a4788606bd504f263ff53
turepinar <yilmazerp@gmail.com>
2023-12-01 16:22:02 -0600

    2.nd commit for merge conflict

--- src/test/java/Customs.java
+++ src/test/java/Customs.java
@@ -1,5 +1,6 @@
 public class Customs {
     public static void main(String[] args) {
         System.out.println("Customs class created");
+        System.out.println("merge conflict icin ucuncu satir");
     }
 }
