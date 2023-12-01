0d17dab5aee7fe76180b37e58ee1971878a52945
turepinar <yilmazerp@gmail.com>
2023-12-01 10:22:21 -0600

    Login page change to see how sout and note will reflect in commit and pull request

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -4,5 +4,7 @@ public class Login{
         System.out.println("Login");
         System.out.println("First Feature created for Login");
         System.out.println("NEw feature for login WO password");
+        System.out.println("After a change in file, nothing shouw up in commit and pull request");
+        //Then I added new sout and new note to see how it will appear in commit.
     }
 }
