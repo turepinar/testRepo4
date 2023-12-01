72be42e9c9782a1169b8546723241d3a505f9c4e
turepinar <yilmazerp@gmail.com>
2023-12-01 16:17:05 -0600

    1st commit for merge conflict

--- src/test/java/Customs.java
+++ src/test/java/Customs.java
@@ -1,5 +1,7 @@
 public class Customs {
     public static void main(String[] args) {
         System.out.println("Customs class created");
+        System.out.println("MErge conflict icin ilk satirim");
+        System.out.println("merge conflict icin ikinci satirim");
     }
 }
