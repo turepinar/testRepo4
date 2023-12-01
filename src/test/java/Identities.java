1d887ae651585d39e7da8357366e1157b7b1dfcc
turepinar <yilmazerp@gmail.com>
2023-12-01 13:59:10 -0600

    third commit to Identities

--- src/test/java/Identities.java
+++ src/test/java/Identities.java
@@ -4,5 +4,6 @@ public class Identities {
     public static void main(String[] args) {
         System.out.println("Identities class created");
         System.out.println("Second commit added");
+        System.out.println("Third commit added to identities");
     }
 }
