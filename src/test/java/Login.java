eb2ec77646508f2ff3c96eddcf09087026071078
turepinar <yilmazerp@gmail.com>
2023-12-01 10:36:43 -0600

    triple commits reflection to pull request

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -6,5 +6,6 @@ public class Login{
         System.out.println("NEw feature for login WO password");
         System.out.println("After a change in file, nothing shouw up in commit and pull request");
         //Then I added new sout and new note to see how it will appear in commit.
+        //One more change occured  for seeing how it will appear in commit when we have multiple commits
     }
 }
