0dd15c12d94b6b6c653580ca6988845532041bda
turepinar <yilmazerp@gmail.com>
2023-12-01 12:08:50 -0600

    Second commit added to identities

--- src/test/java/Identities.java
+++ src/test/java/Identities.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Identities {
     public static void main(String[] args) {
         System.out.println("Identities class created");
+        System.out.println("Second commit added");
     }
 }
